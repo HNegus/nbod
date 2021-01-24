6731848372af008f98ebeffcdb69afb3124f5069
HNegus <henok.ghebrenigus@eyp.nl>
2021-01-24 19:26:34 +0100

    testing

--- install.sh
+++ install.sh
@@ -5,23 +5,28 @@ LDIR=$ROOT/lib
 BUILD_DIR=$ROOT/build
 LSOURCES=$ROOT/libsrc
 
+GLEW_SRC=$LSOURCES/glew-2.1.0/build/cmake
 GLEW_SRC_FILE=$LSOURCES/glew/glew.c
 GLFW_SRC=$LSOURCES/glfw-3.3.2
 
 mkdir -p $LDIR
 
 echo building GLEW
-cc -c -o $BUILD_DIR/glew.o $GLEW_SRC_FILE
-ar rcs $LDIR/libglew.a $BUILD_DIR/glew.o
+cd $BUILD_DIR
+cmake $GLEW_SRC
+cd $BUILD_DIR
+make glew_s
+cp lib/libGLEW.a $LDIR
+rm -rf $BUILD_DIR/*
 echo done
 echo
 
-echo Building GLFW
-cd $BUILD_DIR
-cmake -DBUILD_SHARED_LIBS=OFF $GLFW_SRC
-cd $BUILD_DIR/src
-make
-cp libglfw3.a $LDIR
+# echo Building GLFW
+# cd $BUILD_DIR
+# cmake -DBUILD_SHARED_LIBS=OFF $GLFW_SRC
+# cd $BUILD_DIR/src
+# make
+# cp libglfw3.a $LDIR
 
 # cp $(BUILD_DIR)/src/libglfw3.a $(LDIR)
 
